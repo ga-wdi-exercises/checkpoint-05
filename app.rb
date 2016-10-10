26470c7670636cb9b39703616df5d4b48a2ef2cf
Khoi Le <khoileee89@gmail.com>
2016-10-10 09:37:04 -0400

    Functional CR

--- app.rb
+++ app.rb
@@ -5,4 +5,13 @@ require_relative 'models/car'
 
 get '/cars' do
   @cars = Car.all
+  erb :"/cars/index"
+end
+
+get '/cars/new' do
+  erb :"/cars/new"
+end
+post '/cars' do
+  Car.create(params[:car])
+  redirect '/cars'
 end
