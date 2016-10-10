96d630a14951668306634f309c1b6e2a85140f5f
Nick Oki <nick.oki@live.com>
2016-10-10 09:44:33 -0400

    Part 3 + Part 4

--- app.rb
+++ app.rb
@@ -8,7 +8,30 @@ get '/cars' do
   erb :"cars/index"
 end
 
+get '/cars/:id' do
+  @car = Car.find params[:id]
+  erb :"cars/show"
+end
+
+get '/cars/:id/edit' do
+  @car = Car.find params[:id]
+  erb :"cars/edit"
+end
+
 post '/cars' do
   @car = Car.create params[:car]
+  redirect '/cars'
+end
+
+
+put '/cars/:id' do
+  @car = Car.find params[:id]
+  @car.update params[:car]
+  redirect "/cars/#{@car.id}"
+end
+
+delete '/cars/:id' do
+  @car = Car.find params[:id]
+  @car.destroy
   redirect '/cars'
 end
