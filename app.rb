a4eab0f6db857131bd73b7b5e116009148c8f760
Michael Lai <mike24567@gmail.com>
2016-10-10 09:45:50 -0400

    Finished Part 3

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
 require_relative 'db/connection'
 require_relative 'models/car'
 
 get '/cars' do
   @cars = Car.all
   erb :"car/index"
+end
+
+get '/cars/new' do
+  erb :"car/new"
+end
+
+post "/cars" do
+  Car.create(params[:car])
+  redirect "/cars"
 end
