80cfd180acd7b9c17cff4d78fc5135f803db5934
Andrew Whitley <awhitley1233@gmail.com>
2016-10-10 09:40:55 -0400

    finished all

--- app.rb
+++ app.rb
@@ -5,4 +5,39 @@ require_relative 'models/car'
 
 get '/cars' do
   @cars = Car.all
+  erb :"cars/index"
+end
+
+post '/cars' do
+  @car = Car.create(
+    make: params[:make],
+    model: params[:model],
+    year: params[:year],
+    transmission: params[:transmission],
+    color: params[:color]
+  )
+  redirect '/cars'
+end
+
+get '/cars/:id/edit' do
+  @car = Car.find(params[:id])
+  erb :"cars/edit"
+end
+
+put '/cars/:id' do
+  @car = Car.find(params[:id])
+  @car.update(
+    make: params[:make],
+    model: params[:model],
+    year: params[:year],
+    transmission: params[:transmission],
+    color: params[:color]
+  )
+  redirect '/cars'
+end
+
+delete '/cars/:id' do
+  @car = Car.find(params[:id])
+  @car.destroy
+  redirect '/cars'
 end
