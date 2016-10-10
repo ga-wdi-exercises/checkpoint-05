47eedeb89da5126f14555529f39942a1539b6a84
Latoya Watson <latoyamwatson@gmail.com>
2016-10-10 09:57:59 -0400

    watson checkout-05

--- app.rb
+++ app.rb
@@ -3,6 +3,20 @@ require 'sinatra/reloader'
 require_relative 'db/connection'
 require_relative 'models/car'
 
+get '/' do
+  "hello world"
+end
+
 get '/cars' do
   @cars = Car.all
+  erb :index
+end
+
+get '/cars/new' do
+  erb :new
+end
+
+post '/cars' do
+  @car = Car.create (params[:car])
+  redirect '/cars'
 end
