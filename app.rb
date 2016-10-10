2b3caa74f19623b427bc47e0848762d9eacac066
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-10-10 09:28:04 -0400

    index.erb

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
 require_relative 'db/connection'
 require_relative 'models/car'
 
 get '/cars' do
   @cars = Car.all
+  erb :index
 end
