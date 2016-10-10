1c902769978c718b3697870ee293c331fb34546a
Barrett Cope <barrettcope@gmail.com>
2016-10-10 09:52:27 -0400

    Create works

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ require 'sinatra/reloader'
 require_relative 'db/connection'
 require_relative 'models/car'
 
+get '/' do
+  redirect '/cars'
+end
+
 get '/cars' do
   @cars = Car.all
+  erb :'index'
+end
+
+post '/cars' do
+  @car = Car.create(params[:car])
+  redirect '/'
 end
