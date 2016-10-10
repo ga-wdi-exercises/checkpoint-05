5122ef4df8f83873405e0a4635bc5b91b1933757
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-10-10 09:34:13 -0400

    progress 2

--- app.rb
+++ app.rb
@@ -5,4 +5,14 @@ require_relative 'models/car'
 
 get '/cars' do
   @cars = Car.all
+  erb :"car/index"
+end
+
+get '/car/new' do
+  erb :"car/new"
+end
+
+post '/car' do
+  @car = Car.create(params[:car, :model, :color, :transmission, :make])
+  redirect "/car/#{@car.id}"
 end
