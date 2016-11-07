ebe8ab3ebe57b531dc466396fd76a7262733d339
Kevin Herrera <1herrerake@gmail.com>
2016-11-07 10:03:27 -0500

    quiz

--- posts/db/migrate/20161107134741_create_posts.rb
+++ posts/db/migrate/20161107134741_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-
-      t.timestamps
+      t.string :body
+      t.string :author
+      t.timestamps null: false
     end
   end
 end
--- posts/db/seeds.rb
+++ posts/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+Post.create([
+  {body: "my life", author: "kevin herrera"}
+  ])
