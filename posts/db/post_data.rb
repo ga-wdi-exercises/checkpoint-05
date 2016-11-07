6a0f56cfea3875f0bcdc50b0b89bbdb84f2027f4
Sam Catherman <Sam.Catherman@gmail.com>
2016-11-07 10:01:38 -0500

    initial commit, hit a brick wall with routing before I could finish

---
+++ posts/db/post_data.rb
@@ -0,0 +1,17 @@
+# def get_post_data
+#   post_data ={
+#     "Post1" => {
+#       body: "This is the body of post 1",
+#       author: "Sam C",
+#     },
+#     "Post2" => {
+#       body: "This is the body of post 2",
+#       author: "Sam D",
+#     },
+#     "Post3" => {
+#       body: "This is the body of post 3",
+#       author: "Sam E",
+#     }
+#   }
+#   return post_data
+# end
