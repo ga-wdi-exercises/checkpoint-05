46ce844e87397cc47e5a0bb0ba5b11b103a6fd90
Samuel Clements <sdkclements@gmail.com>
2017-02-23 09:28:34 -0500

    fixed controllerindex because i dont know where to place, do, because im an idiot

--- todo_app/app/controllers/todo_controller.rb
+++ todo_app/app/controllers/todo_controller.rb
@@ -1,10 +1,13 @@
 class TodoController < ApplicationController
   def index
+  	@todos = Todo.all
   end
 
   def show
+  	@todo = Todo.find(params[:id])
   end
 
   def new
+  	@todo = Todo.new(params[:todo])
   end
 end
