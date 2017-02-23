289fc08ff70dbef14a988d53aad6dad59945a4aa
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-02-23 09:47:53 -0500

    full functionality achieved

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -4,10 +4,21 @@ class TodosController < ApplicationController
   end
 
   def show
-
+    @todo = Todo.find(params[:id])
   end
 
   def new
-
+    @new_todo = Todo.new
   end
+  def create
+    @new_todo = Todo.new(todo_params)
+    @new_todo.is_completed = false
+    @new_todo.save
+    redirect_to '/'
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:content)
+    end
 end
