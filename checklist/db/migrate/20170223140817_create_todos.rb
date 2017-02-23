b9dca9c237c61becd7e40d729676ebf94ce31e4f
Eva Lai <evalai.dc@gmail.com>
2017-02-23 09:25:22 -0500

    created seed files and schema, able to run rails server

--- checklist/db/migrate/20170223140817_create_todos.rb
+++ checklist/db/migrate/20170223140817_create_todos.rb
@@ -1,6 +1,9 @@
 class CreateTodos < ActiveRecord::Migration[5.0]
   def change
     create_table :todos do |t|
+      t.string :content
+      t.boolean :is_completed
+      
     end
   end
 end
--- checklist/db/seeds.rb
+++ checklist/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.destroy_all
+
+scribble = Todo.create!(content: "Scribble Homework", is_completed: true)
+outcomes = Todo.create!(content: "Outcomes Homework", is_completed: false)
