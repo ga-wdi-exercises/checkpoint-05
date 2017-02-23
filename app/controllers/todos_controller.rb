9727924f0b05e06f77f96b4764bbca6822c18f98
Erica Norby <ericanorby@gmail.com>
2017-02-23 09:51:54 -0500

    finished checkpoint

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,7 +9,20 @@ class TodosController < ApplicationController
   end
 
   def new
+    @todo = Todo.new
+  end
 
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.is_completed = false
+    @todo.save
+    redirect_to todos_path
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:content)
   end
 
 end
