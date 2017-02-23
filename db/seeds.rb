e4cf4191f647dd9887393f9653020fbaf60aa5a9
Erica Norby <ericanorby@gmail.com>
2017-02-23 09:41:22 -0500

    added routes, index and show views

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Todo.destroy_all
+
 Todo.create(content: "Clean bedroom", is_completed: false)
 Todo.create(content: "File tax returns", is_completed: false)
