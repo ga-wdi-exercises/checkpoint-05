6edd3c9d90496eaca5b0dc8671191425c24f080e
Vince Campanale <vcampanale0294@gmail.com>
2017-02-23 09:35:20 -0500

    can create new todo - mvp complete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,4 +9,19 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
   end
 
+  #new
+  def new
+    @todo = Todo.new
+  end
+
+  #create
+  def create
+    @todo = Todo.create!(todo_params)
+    redirect_to todo_path(@todo)
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:content, :is_completed)
+  end
 end
