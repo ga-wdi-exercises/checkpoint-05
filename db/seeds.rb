19a46e8f84c055134cddd4ac24ef392f22c010ad
Erica Norby <ericanorby@gmail.com>
2017-02-23 09:13:11 -0500

    completed part 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.create(content: "Clean bedroom", is_completed: false)
+Todo.create(content: "File tax returns", is_completed: false)
