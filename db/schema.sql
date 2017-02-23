d8972c3e05fef59c19e0d940b031df2f06247805
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-23 09:17:59 -0500

    generating the schema

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS songs           CASCADE;
+DROP TABLE IF EXISTS artists         CASCADE;
+
+create table content (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+);
+
+CREATE TABLE is_completed (
+  id SERIAL PRIMARY KEY,
+  status VARCHAR(255),
+);
