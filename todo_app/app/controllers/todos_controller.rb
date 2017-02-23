d5fb7e7f19edb8f47ec9461cd846773d690d8d1a
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-23 09:44:54 -0500

    added controller for todos

---
+++ todo_app/app/controllers/todos_controller.rb
@@ -0,0 +1,13 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+end
