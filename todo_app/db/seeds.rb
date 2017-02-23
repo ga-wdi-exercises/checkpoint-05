0e4c74e6f4dd210c5f8a3c1d0af6c0868f416290
xgironx <xgironx@gmail.com>
2017-02-23 09:54:07 -0500

    got models, tables seeded

--- todo_app/db/seeds.rb
+++ todo_app/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Todo.destroy_all
+
+dumbThingIGottaDo11 = Todo.create(content:"eat crow", is_completed: false)
+dumbThingIGottaDo11 = Todo.create(content:"grovel", is_completed: false)
