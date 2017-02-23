fba2637800e00464cbeae368f21054188de2a497
Nat Carter <n.goldman.carter@gmail.com>
2017-02-23 09:45:07 -0500

    drafted views; updated controller to initialized is_completed to false

--- readme_copy.md
+++ readme_copy.md
@@ -1,7 +1,3 @@
-index view
-show view
-new view
-
 ***Bonus***
 
 Implement create, destroy, edit and update features
--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -12,12 +12,12 @@ class TodosController < ApplicationController
   end
 
   def create
-    @todo = Todo.create(todo_params)
+    @todo = Todo.create(todo_params.merge(is_completed: false))
     redirect_to todo_path(@todo)
   end
 
   private
   def todo_params
-    params.require(:todo).permit(:content, :is_completed)
+    params.require(:todo).permit(:content)
   end
 end
