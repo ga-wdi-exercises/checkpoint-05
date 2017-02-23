cc3fdb1b8376c2bd69b47c7684a7d824e8c03ee3
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-23 09:20:17 -0500

    part 1 finished

--- todo_app/db/seeds.rb
+++ todo_app/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.create(content: 'Content of the first todo.', is_completed: 'true')
+Todo.create(content: 'Content of the second todo.', is_completed: 'false')
