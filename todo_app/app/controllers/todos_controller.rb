d13343c026c8c0865da6ca2b87ab468e65a2e7ee
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-23 09:35:58 -0500

    part 2 finished

---
+++ todo_app/app/controllers/todos_controller.rb
@@ -0,0 +1,30 @@
+class TodosController < ApplicationController
+
+  # index
+  def index
+    @todos = Todo.all
+  end
+
+  #show
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  # new
+  def new
+    @todo = Todo.new
+  end
+
+  # create
+  def create
+    @todo = Todo.create!(todo_params)
+
+    redirect_to todo_path(@todo)
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:content, :is_completed)
+    end
+
+end
