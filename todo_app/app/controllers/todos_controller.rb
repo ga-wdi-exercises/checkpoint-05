a225d2b113e64426a85e673196beaed4d4c837d6
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-02-23 11:43:00 -0500

    completed

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -1,24 +1,24 @@
 class TodosController < ApplicationController
   # index
   def index
-    @todos = todo.all
+    @todos = Todo.all
   end
 
   # new
   def new
-    @todo = todo.new
+    @todo = Todo.new
   end
 
   # create
   def create
-    @todo = todo.create!(todo_params)
+    @todo = Todo.create!(todo_params)
 
     redirect_to todo_path(@todo)
   end
 
   #show
   def show
-    @todo = todo.find(params[:id])
+    @todo = Todo.find(params[:id])
   end
   private
     def todo_params
