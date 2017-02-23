63aa938d8e376fa42477062fb594bea2aed29fa5
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-02-23 09:52:35 -0500

    updated files

--- todo_app/db/seeds.rb
+++ todo_app/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require_relative './todo_app.rb'
+
+Todo.destroy_all
+
+
+todo_data = get_todo_data()
+
+
+song_data.each_pair do |artist_name, songs|
+  info = artist_data[artist_name]
+  current_artist = Artist.create!({
+    name:         info[:name],
+    task:    info[:task],
+    status:  info[:status]
+  })
+end
