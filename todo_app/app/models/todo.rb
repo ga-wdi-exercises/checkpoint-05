87aefb2ed70099916653582e8fd0a7a3c3d57c5a
Abrahim <aeaghannam@aol.com>
2017-04-24 09:52:56 -0400

    abe's attempt at C5

---
+++ todo_app/app/models/todo.rb
@@ -0,0 +1,6 @@
+# Task 3 - Define a Todo model and create a corresponding migration.
+# It should have two attributes: content (string) and is_completed (boolean).
+
+
+class Person < ActiveRecord::Base
+end
