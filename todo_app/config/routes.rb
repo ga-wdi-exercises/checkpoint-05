33b01b20a409613f0544b966a00b5548262a417d
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-04-24 09:51:29 -0400

    new commits

--- todo_app/config/routes.rb
+++ todo_app/config/routes.rb
@@ -1,3 +1,6 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Todos::Application.routes.draw do
+  get 'Todo/index'
+  root "todos#index"
+  resources :content, only: [:index, :show]
+  resources :is_completed, only: [:index, :show]
 end
