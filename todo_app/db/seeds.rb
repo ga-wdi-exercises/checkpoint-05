ed4a183018ee2ed5940c40df0c9cff9aec07e847
Conner Johnson <Conner.johnson33@gmail.com>
2017-04-24 09:26:51 -0400

    seed migration

--- todo_app/db/seeds.rb
+++ todo_app/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.destroy_all
+
+"Take out trash" = Todo.create(content: "Take trash to the curb", is_completed: false)
+"PayBills" = Todo.create(content: "Pay water and gas bill", is_completed: false)
+"Feed dog" = Todo.create(content: "Feed dog at 8 am", is_completed: false)
+"Wake up" = Todo.create(content: "Wake up at 7am", is_completed: false)
