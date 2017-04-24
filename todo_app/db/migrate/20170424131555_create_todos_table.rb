bbec47d1e1030579753b5d799c8786d4daa36468
Erin <erinedawson@gmail.com>
2017-04-24 09:24:46 -0400

    create schema

--- todo_app/db/migrate/20170424131555_create_todos_table.rb
+++ todo_app/db/migrate/20170424131555_create_todos_table.rb
@@ -1,6 +1,8 @@
 class CreateTodosTable < ActiveRecord::Migration[5.0]
   def change
     create_table :todos_tables do |t|
+      t.string :content
+      t.boolean :is_completed
     end
   end
 end
