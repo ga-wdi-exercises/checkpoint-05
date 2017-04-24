ad7ec24adb649a91b15bf650f216fd44ae357b3d
Whitney <whitney.purdum@gmail.com>
2017-04-24 09:59:19 -0400

    update

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -8,4 +8,18 @@ def index
     @todo = Todo.find(params[:id])
   end
 
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+  @todo = Todo.create!(todo_params)
+  redirect_to todo_path
+  end
+
+  private
+  def todo_params
+    params.require(:content).permit(:content)
+  end
+
 end
