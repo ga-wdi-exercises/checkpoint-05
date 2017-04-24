ab9d3403f73b878eee4ff20e9860a924a9d7bc22
Jonathan Wright <jonwright.va@gmail.com>
2017-04-24 09:53:08 -0400

    created views for controller, views are working

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -11,3 +11,13 @@ class TodosController < ActionController::Base
 
 	end
 end
+
+#    Prefix Verb   URI Pattern               Controller#Action
+#     todos GET    /todos(.:format)          todos#index
+#           POST   /todos(.:format)          todos#create
+#  new_todo GET    /todos/new(.:format)      todos#new
+# edit_todo GET    /todos/:id/edit(.:format) todos#edit
+#      todo GET    /todos/:id(.:format)      todos#show
+#           PATCH  /todos/:id(.:format)      todos#update
+#           PUT    /todos/:id(.:format)      todos#update
+#           DELETE /todos/:id(.:format)      todos#destroy
