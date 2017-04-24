37eb6a84c1c3d750281d5f0b9fe547bc98075e9e
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-24 09:55:00 -0400

    create show and new pages, various other updates

---
+++ todo_app/app/controllers/todos_controller.rb
@@ -0,0 +1,23 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    redirect_to todos_path(@todo)
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:content)
+  end
+end
--- todo_app/db/schema.rb
+++ todo_app/db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170424131615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.string  "content"
+    t.boolean "is_completed"
+  end
 
 end
