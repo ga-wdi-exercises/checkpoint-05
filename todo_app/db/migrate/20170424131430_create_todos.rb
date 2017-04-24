e4bc2d0c12d2a497051c6e80adb9567c67cfd662
Whitney <whitney.purdum@gmail.com>
2017-04-24 09:51:50 -0400

    update

--- todo_app/db/migrate/20170424131430_create_todos.rb
+++ todo_app/db/migrate/20170424131430_create_todos.rb
@@ -1,6 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[5.0]
   def change
     create_table :todos do |t|
+      t.string :content
+      t.boolean :is_complete, default: false
     end
   end
 end
--- todo_app/db/seeds.rb
+++ todo_app/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.destroy_all
+
+todo_one = Todo.create(content: "Clean room", is_complete: false)
+todo_two = Todo.create(content: "Do Code Wars", is_complete: false)
+todo_three = Todo.create(content: "Update resume", is_complete: false)
+todo_four = Todo.create(content: "Call mom", is_complete: false)
+todo_five = Todo.create(content: "Send birthday card", is_complete: false)
