57c6d7e34baed54bd3c5181f1498d93870e25078
Andres Arango <andresarangov17@gmail.com>
2017-05-01 19:23:46 -0400

    change routes to resources

--- todo_app/config/routes.rb
+++ todo_app/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "todos/index" to "todos#index"
-  get "todos/show" to "todos#show"
-  get "todos/new" to "todos#new"
+  resources :todos
 end
