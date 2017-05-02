9da7e48571a59aa127340ec304155a511f563cd0
Andres Arango <andresarangov17@gmail.com>
2017-05-01 22:00:31 -0400

    add edit

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -12,11 +12,20 @@ class TodosController < ApplicationController
     @todo = Todo.create(todo_params)
     redirect_to todos_path
   end
+  def edit
+    @todo = Todo.find(params[:id])
+  end
 
   def destroy
     @todo = Todo.find(params[:id])
     @todo.destroy
     redirect_to todos_path
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    redirect_to todo_path(@todo)
   end
 
   private
