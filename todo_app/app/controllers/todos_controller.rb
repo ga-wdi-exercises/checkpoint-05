93aa160390809f4f7eab949aa455fb22e06a55d8
Andres Arango <andresarangov17@gmail.com>
2017-05-01 20:02:29 -0400

    add views and form. finish app

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -6,6 +6,21 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
   end
   def new
+    @todo = Todo.new
+  end
+  def create
+    @todo = Todo.create(todo_params)
+    redirect_to todos_path
+  end
+
+  def destroy
     @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:content, :is_completed)
   end
 end
