242538d90df37f76c4d00804e6d32e0933abf977
Tim Chase <tchase4544@gmail.com>
2017-06-06 09:25:30 -0400

    controller made, route basics made

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+	resources :todo
+  get 'todos/index'
+
+  get 'todos/new'
+
+  get 'todos/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
