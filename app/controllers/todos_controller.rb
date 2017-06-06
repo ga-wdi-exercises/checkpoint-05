bd210e24e05641be4c72c0b10046f3294c096308
Tim Chase <tchase4544@gmail.com>
2017-06-06 09:47:10 -0400

    index,new,create routes and views created

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,6 +8,17 @@ class TodosController < ApplicationController
   end
 
   def create
-  
+  	@todo = Todo.new(todo_params)
+  	if @todo.save
+  		flash[:notice] = "New todo addded"
+  		redirect_to todos_path
+  	else
+  		flash[:alert] = "New todo did not save"
+  		redirect_to new_todo_path
+  	end
   end
+  private
+  	def todo_params
+  		params.require(:todo).permit(:content,:is_completed)
+  	end
 end
