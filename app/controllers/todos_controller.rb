3afda9ced43588bb8b39a8726a4a9e697a63c171
Tim Chase <tchase4544@gmail.com>
2017-06-06 09:35:14 -0400

    views edited

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,13 @@
 class TodosController < ApplicationController
   def index
+  	@todos = Todo.all
   end
 
   def new
+  	@todo = Todo.new
   end
 
   def create
+  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-	resources :todos only: [:index, :new, :create] 
+	get root 'todos#index'
+
+	resources :todos, only: [:index, :new, :create] 
  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
